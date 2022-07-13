39766ad222b5615a8a1190cd723b77f3472b717c
Marc Schot <info@cryptology.hk>
2022-07-13 15:15:56 -0500

    add new vote typegroup

--- src/utils/queries.ts
+++ src/utils/queries.ts
@@ -91,7 +91,7 @@ export const getVoterSinceHeight = (
               OR transactions."asset" @> '{"votes": ["-${delegatePublicKey}"]}' \
               OR transactions."asset"  @> '{"votes": ["+${delegateName}"]}' \
               OR transactions."asset"  @> '{"votes": ["-${delegateName}"]}' \
-              OR transactions."asset"  @> '{"votes": ["${delegateName}"]}' \
+              OR transactions."asset"->"votes"  @> '"${delegateName}"' \
           ) \
           AND blocks.height >= ${startBlockHeight} ORDER BY blocks."height" ASC;`;
 };
