9bc79aa18819dba397a97c4300d0169372f4b937
Marc Schot <info@cryptology.hk>
2022-07-13 11:53:47 -0500

    upate logger

--- src/services/logger.ts
+++ src/services/logger.ts
@@ -3,8 +3,12 @@ import pino from "pino";
 export const logger = pino({
     name: "Cryptology TBW",
     safe: true,
-    prettyPrint: {
-        translateTime: true,
-        ignore: "pid,hostname",
+    transport: {
+        target: 'pino-pretty',
+        options: {
+            colorize: true,
+            translateTime: true,
+            ignore: "pid,hostname"
+        }
     },
 });
