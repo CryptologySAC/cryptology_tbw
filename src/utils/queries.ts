548a3f2323986482db815e5ffade674cbefa5542
Marc Schot <info@cryptology.hk>
2022-07-13 14:37:33 -0500

    add new vote typegroup

--- src/utils/queries.ts
+++ src/utils/queries.ts
@@ -83,7 +83,6 @@ export const getVoterSinceHeight = (
           blocks."height" \
           FROM transactions INNER JOIN blocks ON blocks."id" = transactions."block_id"
           WHERE \
-          
           (( \
           transactions."type" = 3 AND transactions."type_group" = 1 \
           AND ( \
@@ -96,8 +95,7 @@ export const getVoterSinceHeight = (
           ( \
           transactions."type" = 2 AND transactions."type_group" = 2 \
           AND transactions."asset" @> '{votes: ["${delegateName}"]}'
-          )) \
-            
+          )) \            
           AND blocks.height >= ${startBlockHeight} ORDER BY blocks."height" ASC;`;
 };
 
